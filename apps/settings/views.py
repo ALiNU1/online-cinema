4a55cf1f32f777342346ee9d7545b745f8fce121
alinu1 <uwu1343@gmail.com>
2022-08-17 17:29:52 +0600

    Add models in all apps

--- apps/settings/views.py
+++ apps/settings/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from movies.models import Setting
 # Create your views here.
+
+
+def settings(request):
+    setting = Setting.objects.latest('id')
+    context = {
+        'setting' : setting,
+    }
+    return render(request, 'index.html', context)
