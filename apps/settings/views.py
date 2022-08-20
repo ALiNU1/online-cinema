830c3c26d22f0d7532f750b382b0e40a02b2fc94
alinu1 <uwu1343@gmail.com>
2022-08-21 00:51:01 +0600

    fail movie output

--- apps/settings/views.py
+++ apps/settings/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from .models import Setting
-
+from apps.movies.models import Movie
 # Create your views here.
 
 
 def index(request):
     setting = Setting.objects.latest('id')
+    movie = Movie.objects.all().order_by('?')
     context = {
         'setting' : setting,
+        'movie' : movie
     }
     return render(request, 'index.html', context)
